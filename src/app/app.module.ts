292d0be6c089a9af90781136a585c4d070601b77
Diego Armando Jaimes Vázquez. CCORP <diego_jaimes@consucorp.com.mx>
2022-01-30 08:29:10 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { AgregarComponent } from './componentes/agregar/agregar.component';
+import { EditarComponent } from './componentes/editar/editar.component';
+import { FormsModule } from '@angular/forms';
+import { NegocioComponent } from './componentes/negocio/negocio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    AgregarComponent,
+    EditarComponent,
+    NegocioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/componentes/negocio/negocio.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NegocioService } from 'src/app/services/negocio.service';
+
+@Component({
+  selector: 'app-negocio',
+  templateUrl: './negocio.component.html',
+  styleUrls: ['./negocio.component.css']
+})
+export class NegocioComponent implements OnInit {
+
+  lista: any = []
+  id: any
+
+  constructor(
+    private negService: NegocioService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id']
+    this.getNegocio()
+  }
+
+  getNegocio() {
+    
+    console.log(this.id);
+    
+    this.negService.getNegocio(this.id).subscribe(
+      res => {
+        this.lista = res
+        console.log(this.lista.data);
+      },
+      error => console.log(error)
+    )
+  }
+
+}
