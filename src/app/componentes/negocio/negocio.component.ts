90ad618bd2eecd0f3e481ab93c91cf4042f52b70
Diego Armando Jaimes Vázquez. CCORP <diego_jaimes@consucorp.com.mx>
2022-01-30 20:13:39 -0600

    feat: cambios en mapas
    - se cambia navbar
    - se agrega modulo google maps
    - se agrega mask module
    - se agrega soporte para icofonts

--- src/app/componentes/negocio/negocio.component.ts
+++ src/app/componentes/negocio/negocio.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NegocioService } from 'src/app/services/negocio.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-negocio',
@@ -13,25 +14,31 @@ export class NegocioComponent implements OnInit {
   id: any
 
    // Configuración de Google Maps 
-   center = {lat: 18.3356806, lng: -99.5110333};
-   zoom = 20;
-   display?: google.maps.LatLngLiteral;
+   googleUrl: any;
+   saniUrl: any;
 
   constructor(
     private negService: NegocioService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer
   ) { }
+  
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id']
-    this.getNegocio()
+    this.getNegocio(this.id)
+
+    setTimeout(() => {
+      let lat = parseFloat(this.lista[0].latitud)
+      let lng = parseFloat(this.lista[0].longitud)
+      this.googleUrl = 'http://maps.google.com/maps?q=' + lat + ',' + lng + '&z=16&output=embed'
+      console.log(this.googleUrl);
+      this.saniUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.googleUrl)
+    }, 100);
   }
 
-  getNegocio() {
-    
-    console.log(this.id);
-    
-    this.negService.getNegocio(this.id).subscribe(
+  getNegocio(id: any) {
+    this.negService.getNegocio(id).subscribe(
       res => {
         this.lista = res
         console.log(this.lista);
